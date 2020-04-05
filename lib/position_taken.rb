22b5c1a9e5037871c799521c652eff6741e573d3
Ryan Pearce <ryan.pearce@key.me>
2020-04-05 16:09:14 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if board[index] == " "
     false
-  else board[index] "X" or board[index] == "O"
+  elsif (board[index] == "X" || board[index] == "O")
     true
   end
 end
